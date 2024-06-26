f5de35ea65fc79910e072f5eb4c2bf00ea65a465
Gustavo <batistag395@gmail.com>
2024-06-26 17:50:58 -0300

    refatorando karfaklistener em str-consumer

--- str-consumer/src/main/java/com/example/str_consumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/example/str_consumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.example.str_consumer.listeners;
 
+import com.example.str_consumer.Custom.StrConsumerCustomListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -8,26 +9,38 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class StrConsumerListener {
-
-    @KafkaListener(groupId = "group-1",
-            topicPartitions = {
-                @TopicPartition(topic = "str-topic", partitions = {"0"}),
-            },
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message){
         log.info("LOG ::: Recieve message {}", message);
     }
-    @KafkaListener(groupId = "group-0",
-            topicPartitions = {
-                    @TopicPartition(topic = "str-topic", partitions = {"1"})
-            },
-            containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void create(String message){
         log.info("CREATE ::: Recieve message {}", message);
     }
 
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     public void history(String message){
         log.info("HISTORY ::: Recieve message {}", message);
     }
+//    @KafkaListener(groupId = "group-0",
+//            topicPartitions = {
+//                @TopicPartition(topic = "str-topic", partitions = {"0"}),
+//            },
+//            containerFactory = "strContainerFactory")
+//    public void log(String message){
+//        log.info("LOG ::: Recieve message {}", message);
+//    }
+//    @KafkaListener(groupId = "group-1",
+//            topicPartitions = {
+//                    @TopicPartition(topic = "str-topic", partitions = {"1"})
+//            },
+//            containerFactory = "strContainerFactory")
+//    public void create(String message){
+//        log.info("CREATE ::: Recieve message {}", message);
+//    }
+//
+//    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+//    public void history(String message){
+//        log.info("HISTORY ::: Recieve message {}", message);
+//    }
 }
